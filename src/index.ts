17994bb21a48e7900dfe83e8ef319c899a07ebdd
Dhyey Moliya <dhyeymoliya@gmail.com>
2021-04-30 18:36:13 +0530

    Added library code

---
+++ src/index.ts
@@ -0,0 +1,311 @@
+import type { Writable, Unsubscriber } from 'svelte/store';
+import { get, writable } from 'svelte/store';
+import type { ObjectSchema, ValidationError } from 'yup';
+
+type FormState = {
+    [key: string]: {
+        _touched?: boolean,
+        _errors?: string[],
+        [key: string]: any,
+    }
+};
+
+type FormConfigInput<Data = any> = {
+    initialValues: Data,
+    validationSchema?: ObjectSchema<any>,
+    validationClasses?: {
+        valid?: string,
+        invalid?: string,
+        showValid?: boolean,
+        showInvalid?: boolean,
+    },
+};
+
+function isNullish(value) {
+    return value === undefined || value === null;
+}
+
+function isEmpty(object) {
+    return isNullish(object) || Object.keys(object).length <= 0;
+}
+
+const createState = (initialValues: any, state: Writable<FormState>, validationSchema: ObjectSchema<any>) => {
+    if (validationSchema) {
+        state.set(createValidatedState(initialValues, validationSchema.fields));
+    } else {
+        state.set(createRawState(initialValues));
+    }
+}
+
+const createValidatedState = <Data = any>(data: Data, schema: ObjectSchema<any>, state: FormState = {}) => {
+    for (let key in schema) {
+        if (schema[key].type === 'array') {
+            const values = ((data?.[key] || []) as any[]);
+            state[key] = values.map(
+                (value, index) => createValidatedState(value, schema[key].innerType.fields, Object.assign({}, state[key]?.[index]))
+            );
+            state[key]._errors = [];
+        } else if (schema[key].type === 'object' && !isEmpty(schema[key].fields)) {
+            state[key] = {
+                ...createValidatedState(data[key], schema[key].fields, Object.assign({}, state[key])),
+                _errors: [],
+            };
+        } else {
+            state[key] = {
+                _touched: state?.[key]?._touched || false,
+                _errors: [],
+            };
+        }
+    }
+    return state;
+}
+
+const createRawState = <Data>(data: Data, state: FormState = {}): FormState => {
+    for (let key in data) {
+        if (Array.isArray(data[key])) {
+            const values = (data[key] || []) as any[];
+            state[key] = values.map(
+                (value, index) => createRawState(value, Object.assign({}, state[key]?.[index]))
+            );
+            state[key]._errors = [];
+        } else if (typeof data[key] === 'object') {
+            state[key] = {
+                ...createRawState(data[key], Object.assign({}, state[key])),
+                _errors: [],
+            };
+        } else {
+            state[key] = {
+                _touched: state?.[key]?._touched || false,
+                _errors: [],
+            };
+        }
+    }
+
+    return state;
+}
+
+const validate = <Data = any>(form: Writable<Data>, schema: ObjectSchema<any>, state: Writable<FormState>, touchValid = false, touchInvalid = false) => {
+    try {
+        schema.validateSync(get(form), { abortEarly: false });
+        clearErrors(state, touchValid);
+        return true;
+    } catch (error) {
+        setErrors(state, error, touchValid, touchInvalid);
+        return false;
+    }
+}
+
+const clearErrors = (state: Writable<FormState> | any, touch = false, inner = false, update = true) => {
+    try {
+        let data = !inner ? get(state) : state;
+        for (let key in data) {
+            if (key !== '_touched' && key !== '_errors' && key !== '_index') {
+                if (Array.isArray(data[key])) {
+                    let values = (data[key] || []) as any[];
+                    data[key] = values.map(
+                        value => clearErrors(value, touch, true, update)
+                    );
+                    data[key]._touched = typeof data[key]?._touched === 'boolean' && !touch ? data[key]._touched : touch;
+                    data[key]._errors = [];
+                } else if (typeof data[key] === 'object') {
+                    data[key] = {
+                        ...clearErrors(data[key], touch, true, update),
+                        _touched: typeof data[key]?._touched === 'boolean' && !touch ? data[key]._touched : touch,
+                        _errors: [],
+                    };
+                } else {
+                    data[key] = {
+                        _touched: typeof data[key]?._touched === 'boolean' && !touch ? data[key]._touched : touch,
+                        _errors: [],
+                    };
+                }
+            }
+        }
+        if (!inner) {
+            update && state.set(data as FormState);
+            return data;
+        } else {
+            return data;
+        }
+    } catch (error) {
+        console.error(error);
+        return state;
+    }
+}
+
+const setErrors = (state: Writable<FormState>, mainError: ValidationError, touchValid = false, touchInvalid = false) => {
+    try {
+        let _state = get(state);
+        _state = clearErrors(_state, touchValid, true, false);
+        for (let error of mainError.inner) {
+            let segments = error.path.toString().match(/[^.[\]]+/g) || [];
+
+            let obj = _state;
+            if (segments.length > 1) {
+                const objSegments = segments.slice(0, segments.length - 1);
+                let exists = true;
+                for (let segment of objSegments) {
+                    if (!obj[segment]) {
+                        exists = false;
+                        break;
+                    }
+                    obj = obj[segment];
+                }
+                if (!exists) {
+                    break;
+                }
+            }
+
+            let lastSegment = segments[segments.length - 1];
+            if (!obj[lastSegment]) { obj[lastSegment] = { touched: false, _errors: [] }; }
+            if (!obj[lastSegment]._errors) { obj[lastSegment]._errors = []; }
+            obj[lastSegment]._errors.push(error.message);
+            obj[lastSegment]._touched = typeof obj[lastSegment]._touched === 'boolean' && !touchInvalid ? obj[lastSegment]._touched : touchInvalid;
+        }
+        state.set(_state);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const updateFieldTouched = (name: string, state: Writable<FormState>) => {
+    let segments = name.toString().match(/[^.[\]]+/g) || [];
+    let _state = get(state);
+    let obj = _state;
+    let exists = true;
+    if (segments.length > 1) {
+        const objSegments = segments.slice(0, segments.length - 1);
+        for (let segment of objSegments) {
+            if (!obj[segment]) {
+                exists = false;
+                break;
+            }
+            obj = obj[segment];
+        }
+    }
+
+    if (exists) {
+        let lastSegment = segments[segments.length - 1];
+
+        if (obj[lastSegment]) {
+            obj[lastSegment]._touched = true;
+        }
+    }
+    state.set(_state);
+}
+
+const updateState = (form: Writable<any>, validationSchema: ObjectSchema<any>, state: Writable<FormState>) => {
+    let _state = get(state);
+    if (validationSchema) {
+        _state = createValidatedState(get(form), validationSchema.fields, JSON.parse(JSON.stringify(_state)))
+    } else {
+        _state = createRawState(get(form), JSON.parse(JSON.stringify(_state)));
+    }
+    state.set(_state);
+}
+
+const getFieldState = (name: string, $state: FormState) => {
+    let segments = name.toString().match(/[^.[\]]+/g) || [];
+    let obj = $state;
+
+    const objSegments = segments.slice(0, segments.length - 1);
+    for (let segment of objSegments) {
+        if (!obj[segment]) {
+            return null;
+        }
+        obj = obj[segment];
+    }
+    let lastSegment = segments[segments.length - 1];
+    return obj[lastSegment] || null;
+}
+
+export function createForm<Data>({ initialValues, validationSchema, validationClasses }: FormConfigInput<Data>) {
+    const form = writable<Data>(initialValues);
+    const state = writable<FormState>({});
+    const isValid = writable<boolean>(false);
+    const isTouched = writable<boolean>(false);
+    const classes = Object.assign({
+        valid: 'is-valid',
+        invalid: 'is-invalid',
+        showValid: true,
+        showInvalid: true,
+    }, validationClasses || {});
+
+    createState(initialValues, state, validationSchema);
+
+    const updateForm = async () => {
+        updateState(form, validationSchema, state);
+        validateForm();
+    };
+
+    const validateForm = (highlight: 'none' | 'errors' | 'all' = 'none') => {
+        isValid.set(validate(form, validationSchema, state, highlight === 'all', highlight === 'all' || highlight === 'errors'));
+    }
+
+    form.subscribe(() => {
+        validationSchema && validateForm();
+    });
+
+    const handleChange = (event: Event) => {
+        const target = event?.target as HTMLInputElement;
+        target.name && updateFieldTouched(target.name, state);
+        isTouched.set(true);
+    }
+
+    const setTouched = (state: boolean) => {
+        isTouched.set(state);
+    }
+
+    const formControl = (node: HTMLInputElement, options: any = {}) => {
+        const changeListender = (event: Event) => {
+            handleChange(event);
+        }
+        let unsubscribeState: Unsubscriber = null;
+
+        const checkValidity = async ($state: FormState) => {
+            const fieldState = getFieldState(node.name, $state);
+            const invalid = fieldState?._touched && !!fieldState?._errors?.length;
+            const valid = fieldState?._touched && !fieldState?._errors?.length;
+            node.classList.remove(classes.valid);
+            node.classList.remove(classes.invalid);
+            if (invalid) {
+                classes.showInvalid && node.classList.add(classes.invalid);
+            } else if (valid) {
+                classes.showValid && node.classList.add(classes.valid);
+            }
+
+        }
+
+        if (['input', 'checkbox', 'radio', 'select', 'textarea'].includes(node.tagName)
+            || node.contentEditable) {
+            node.addEventListener('change', changeListender);
+            node.addEventListener('blur', changeListender);
+            if (node.name) {
+                unsubscribeState = state.subscribe($state => {
+                    checkValidity($state);
+                });
+            }
+        }
+        return {
+            update(options: any = {}) { },
+            destroy() {
+                node.removeEventListener('change', changeListender);
+                node.removeEventListener('blur', changeListender);
+                unsubscribeState && unsubscribeState();
+            }
+        }
+    }
+
+    return {
+        form,
+        state,
+        isValid,
+        isTouched,
+        validateForm,
+        handleChange,
+        updateForm,
+        setTouched,
+        formControl,
+    };
+}
