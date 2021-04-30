65c306c3e6bd84a9da72b4340307422695f3187c
Dhyey Moliya <dhyeymoliya@gmail.com>
2021-04-30 23:43:05 +0530

    Added types folder to git

---
+++ types/index.d.ts
@@ -0,0 +1,36 @@
+import type { Writable } from 'svelte/store';
+import type { ObjectSchema } from 'yup';
+declare type FormState = {
+    [key: string]: {
+        _touched?: boolean;
+        _errors?: string[];
+        [key: string]: any;
+    };
+};
+declare type FormConfigInput<Data = any> = {
+    initialValues: Data;
+    validationSchema?: ObjectSchema<any>;
+    css?: {
+        enabled?: boolean;
+        validClass?: string;
+        invalidClass?: string;
+        useValid?: boolean;
+        useInvalid?: boolean;
+    };
+    validateOnChange?: boolean;
+    validateOnBlur?: boolean;
+};
+export declare function createForm<Data>({ initialValues, validationSchema, css: cssConfig, validateOnChange, validateOnBlur }: FormConfigInput<Data>): {
+    form: Writable<Data>;
+    state: Writable<FormState>;
+    isValid: Writable<boolean>;
+    isTouched: Writable<boolean>;
+    validateForm: (highlight?: 'none' | 'errors' | 'all') => void;
+    handleChange: (event: Event) => void;
+    updateForm: () => Promise<void>;
+    setTouched: (state: boolean) => void;
+    formControl: (node: HTMLInputElement, options?: any) => {
+        destroy(): void;
+    };
+};
+export {};
